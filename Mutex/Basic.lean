a571a85bede99ec2f9d277b3d6b6b840b04dc409
maxwmoir@gmail.com <maxwmoir@gmail.com>
2026-04-23 11:48:32 +1200

    Added Mutex code from old repo

--- Mutex/Basic.lean
+++ Mutex/Basic.lean
@@ -1,3 +1,86 @@
 import Veil
 
-def hello := "world"
+set_option linter.dupNamespace false
+set_option veil.printCounterexamples true
+
+veil module Mutex
+
+type node
+type ticket
+
+instantiate tot : TotalOrder node
+
+open TotalOrder
+
+-- Nodes
+relation critical : node → Prop
+function choosing : node → Prop
+function number   : node → ℕ
+function trying : node → Prop
+
+#gen_state
+
+#print State
+
+after_init {
+  critical N := False;
+  choosing N := False;
+  number N := 0;
+}
+
+#print initialState?
+
+
+action choose (n : node)  = {
+  require ¬ choosing n;
+  choosing n := True;
+
+  -- Find ticket value greater than all others
+  let t_max ← fresh;
+  require ∀ j, j ≠ n → number j < t_max;
+  number n := t_max;
+
+  choosing n := False;
+  trying n := True;
+}
+
+
+
+action enter (i : node) = {
+  require trying i;
+  require number i != 0;
+  require critical i = False;
+
+  require ∀ j, j ≠ i →
+    (choosing j = False) ∧
+    ((number j = 0) ∨
+     (number i < number j) ∨
+     (number i = number j ∧ le i j ∧ ¬ le j i)
+     );
+
+  critical i := True;
+  trying i := False;
+}
+
+action exit (n : node) = {
+  require critical n
+  critical n := False
+  number n := 0
+}
+
+
+safety [mutex] (critical I ∧ critical J) → I = J
+invariant [different_vals]
+  number I ≠ 0 →
+    number I = number J →
+      I = J
+
+-- invariant [critical_lowest]
+--   critical I →
+--     ∀ J, J ≠ I →
+--       number I < number J
+
+#gen_spec
+#check_invariants
+
+end Mutex
