9f2d89ed6c6ed7064d29d31840a0bec813babf19
maxwmoir@gmail.com <maxwmoir@gmail.com>
2026-04-23 12:47:30 +1200

    Change 'n's for 'i's

--- Mutex/Basic.lean
+++ Mutex/Basic.lean
@@ -10,8 +10,8 @@ veil module Mutex
 type node
 type ticket
 
-instantiate tot : TotalOrder node
-open TotalOrder
+instantiate tot : TotalOrderWithMinimum node
+open TotalOrderWithMinimum
 
 -- Nodes
 relation critical : node → Prop
@@ -33,17 +33,17 @@ after_init {
 #print initialState?
 
 
-action choose (n : node)  = {
-  require ¬ choosing n;
-  choosing n := True;
+action choose (i : node)  = {
+  require ¬ choosing i;
+  choosing i := True;
 
   -- Find ticket value greater than all others
   let t_max ← fresh;
-  require ∀ j, j ≠ n → number j < t_max;
-  number n := t_max;
+  require ∀ j, j ≠ i → number j < t_max;
+  number i := t_max;
 
-  choosing n := False;
-  trying n := True;
+  choosing i := False;
+  trying i := True;
 }
 
 
@@ -53,39 +53,43 @@ action enter (i : node) = {
   require number i != 0;
   require critical i = False;
 
+  -- Only allow enter if not choosing
+  require ∀ j, j ≠ i → choosing j = False;
+
+  -- Only allow enter if i holds the smallest non-zero ticket number
   require ∀ j, j ≠ i →
-    (choosing j = False) ∧
-    ((number j = 0) ∨
-     (number i < number j) ∨
-     (number i = number j ∧ le i j ∧ ¬ le j i)
-     );
+    (number j = 0) ∨
+    (number i < number j) ∨
+    (number i = number j ∧ lt i j);
 
   critical i := True;
   trying i := False;
 }
 
 -- Exit CS
-action exit (n : node) = {
-  require critical n
-  critical n := False
-  number n := 0
+action exit (i : node) = {
+  require critical i
+  critical i := False
+  number i := 0
 }
 
 
 
 -- Invariants
 
--- safety [mutex] (critical I ∧ critical J) → I = J
+safety [mutex] (critical I ∧ critical J) → I = J
 
 invariant [different_vals]
   number I ≠ 0 →
     number I = number J →
       I = J
 
--- invariant [critical_lowest]
---   critical I →
---     ∀ J, J ≠ I →
---       number I < number J
+invariant [critical_lowest]
+  critical I →
+     ∀ J, J ≠ I →
+       number I < number J
+
+
 
 #gen_spec
 #check_invariants
